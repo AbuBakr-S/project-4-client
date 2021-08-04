a08d47c3cb4a4a0577135ca6ce2bbe524c0207cb
Abubakar Siddique <abz.siddique@gmail.com>
2021-08-04 17:47:59 +0100

    fix: Improve icons and remove duplicate dashboard

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import Hamburger from 'hamburger-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faFilm, faSignOutAlt, faRegistered, faFileUpload, faBookOpen, faPlusSquare, faChalkboardTeacher } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faSignOutAlt, faList, faPlus, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 import siteLogo from '../../assets/images/site-logo.png'
 import { isAuthenticated, removeToken } from '../../lib/auth'
 
@@ -60,13 +60,11 @@ function Nav() {
       <div ref={menuref} className={sidebarShow ? 'side-nav-menu-container active' : 'side-nav-menu-container'}>
         <ul className="navbar-content-container" onClick={handleSideBar}>
           <li><Link to="/" className="navbar-item" ><FontAwesomeIcon className="fa-items-icon" icon={faHome} />Home</Link></li>
-          <li><Link to="/courses" className="navbar-item"><FontAwesomeIcon className="fa-items-icon" icon={faBookOpen} />Courses</Link></li>
-          <li><Link to="/courses/new" className="navbar-item"><FontAwesomeIcon className="fa-items-icon" icon={faPlusSquare} />New Course</Link></li>
-          <li><Link to="/dashboard" className="navbar-item"><FontAwesomeIcon className="fa-items-icon" icon={faChalkboardTeacher} />Dashboard</Link></li>
-          {/* <li><Link to="/upload" className="navbar-item"><FontAwesomeIcon className="fa-items-icon" icon={faFileUpload} />Upload</Link></li> */}
+          <li><Link to="/courses" className="navbar-item"><FontAwesomeIcon className="fa-items-icon" icon={faList} />Courses</Link></li>
+          <li><Link to="/courses/new" className="navbar-item"><FontAwesomeIcon className="fa-items-icon" icon={faPlus} />New Course</Link></li>
           {!isLoggedIn ?
             <>
-              <li><Link to="/register" className="navbar-item"><FontAwesomeIcon className="fa-items-icon" icon={faRegistered} />Register</Link></li>
+              <li><Link to="/register" className="navbar-item"><FontAwesomeIcon className="fa-items-icon" icon={faUserPlus} />Register</Link></li>
             </>
             :
             <>
